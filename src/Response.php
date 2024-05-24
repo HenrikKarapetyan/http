e658a62f15af1784154ff7d4a5573334b29889e7
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-05-24 16:31:21 +0400

    fixed bug

--- src/Response.php
+++ src/Response.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Henrik\Http;
 
+use Henrik\Contracts\Http\ResponseInterface;
 use Henrik\Contracts\Session\CookieInterface;
 use Henrik\Http\Message\MessageBodyResource;
 use Henrik\Http\Message\MessageBodyString;
@@ -18,52 +19,6 @@ use RuntimeException;
  */
 class Response extends Message implements ResponseInterface
 {
-    /**
-     * Mapping of Status Code to Reason Phrase.
-     */
-    private const STATUS_CODES = [
-        100 => 'Continue',
-        101 => 'Switching Protocols',
-        200 => 'OK',
-        201 => 'Created',
-        202 => 'Accepted',
-        203 => 'Non-Authoritative Information',
-        204 => 'No Content',
-        205 => 'Reset Content',
-        206 => 'Partial Content',
-        300 => 'Multiple Choices',
-        301 => 'Moved Permanently',
-        302 => 'Found',
-        303 => 'See Other',
-        304 => 'Not Modified',
-        305 => 'Use Proxy',
-        307 => 'Temporary Redirect',
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        407 => 'Proxy Authentication Required',
-        408 => 'Request Time-out',
-        409 => 'Conflict',
-        410 => 'Gone',
-        411 => 'Length Required',
-        412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Large',
-        415 => 'Unsupported Media Type',
-        416 => 'Requested range not satisfiable',
-        417 => 'Expectation Failed',
-        500 => 'Internal Server Error',
-        501 => 'Not Implemented',
-        502 => 'Bad Gateway',
-        503 => 'Service Unavailable',
-        504 => 'Gateway Time-out',
-        505 => 'HTTP Version not supported',
-    ];
-
     /**
      * @var int
      */
--- src/ResponseInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Henrik\Http;
-
-interface ResponseInterface
-{
-    public function send(): bool;
-}
